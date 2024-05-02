549a6aa073bb49dd8db6639b398c0c791ba022a5
ovska <ovaskasi@gmail.com>
2024-05-02 19:58:05 +0300

    remove unused regex

--- Pack3r.Core/Tokens.cs
+++ Pack3r.Core/Tokens.cs
@@ -5,23 +5,36 @@ namespace Pack3r;
 
 public static partial class Tokens
 {
+    /// <summary>
+    /// Mapscript keywords that aren't parsed for resources referenced.
+    /// </summary>
     [GeneratedRegex("^(set|create)$", RegexOptions.IgnoreCase | RegexOptions.Singleline, 1000)]
     public static partial Regex UnsupportedMapscript();
 
-    [GeneratedRegex("""^"([^"]+)" "([^"]+)"$""", RegexOptions.Singleline, 1000)]
-    public static partial Regex KeyValuePair();
-
+    /// <summary>
+    /// Matches scripts/*something*.shader
+    /// </summary>
     [GeneratedRegex("""^scripts[/\\][^\.]+.shader$""", RegexOptions.IgnoreCase | RegexOptions.Singleline, 1000)]
     public static partial Regex ShaderPath();
 
+    /// <summary>
+    /// Matches filetypes that should be packaged.
+    /// </summary>
     [GeneratedRegex("""\.(tga|jpg|mdc|md3|ase|shader|wav|roq|skin)$""", RegexOptions.IgnoreCase | RegexOptions.Singleline, 1000)]
     public static partial Regex PackableFile();
 
+    /// <summary>
+    /// Matches quoted/notquoted tokens separated by whitespace.
+    /// </summary>
     [GeneratedRegex("""
         [^\s"]+|"([^"]*)"
         """, RegexOptions.IgnoreCase | RegexOptions.Singleline, 1000)]
     public static partial Regex WhitespaceSeparatedTokens();
 
+    /// <summary>
+    /// Matches the "map" keyword in .arena file.
+    /// </summary>
+    /// <returns></returns>
     [GeneratedRegex("""
         [\s]?map[\s]+"
         """, RegexOptions.IgnoreCase | RegexOptions.Singleline, 1000)]
