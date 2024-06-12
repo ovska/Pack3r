0ced3155f705a7811bdf28499bd81412c86828f6
ovska <ovaskasi@gmail.com>
2024-06-12 10:15:22 +0300

    add tga integrity test

--- Pack3r.Tests/Assets/IntegrityTests.cs
+++ Pack3r.Tests/Assets/IntegrityTests.cs
@@ -11,6 +11,9 @@ public sealed class IntegrityTests
     [Fact]
     public void Should_Verify_Progressive_Jpg()
     {
+        _checker.CheckIntegrity(new FileAsset(Path.GetFullPath($"../../../TestData/etmain/textures/integrity/empty.jpg")));
+        Assert.Empty(_checker.JPGs);
+
         var asset1 = new FileAsset(Path.GetFullPath($"../../../TestData/etmain/fileprio_jpg.pk3dir/textures/smiley.jpg"));
         _checker.CheckIntegrity(asset1);
         Assert.Empty(_checker.JPGs);
@@ -18,15 +21,26 @@ public sealed class IntegrityTests
         // image source: imagekit.io
         var asset2 = new FileAsset(Path.GetFullPath($"../../../TestData/etmain/textures/integrity/progressive.jpg"));
         _checker.CheckIntegrity(asset2);
+        Assert.Single(_checker.JPGs);
         Assert.Equal(asset2.FullPath.NormalizePath(), _checker.JPGs.Single());
     }
 
     [Fact]
     public void Should_Verify_Dangerous_TGA()
     {
+        _checker.CheckIntegrity(new FileAsset(Path.GetFullPath($"../../../TestData/etmain/textures/integrity/empty.tga")));
+        Assert.Empty(_checker.TGAs);
+
         var asset1 = new FileAsset(Path.GetFullPath($"../../../TestData/etmain/fileprio_tga.pk3dir/textures/smiley.tga"));
         _checker.CheckIntegrity(asset1);
         Assert.Empty(_checker.TGAs);
+
+        // image modified from: https://www.hourences.com/texturespage/
+        var asset2 = new FileAsset(Path.GetFullPath($"../../../TestData/etmain/textures/integrity/grass_dark.tga"));
+        _checker.CheckIntegrity(asset2);
+        Assert.Single(_checker.TGAs);
+        Assert.Equal(asset2.FullPath.NormalizePath(), _checker.TGAs.Single());
+
 
     }
 }
